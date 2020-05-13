00d3932601f9ea458683e9809fa7456d67054f27
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-13 07:52:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootjpaspec/SpringbootJpaSpecApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjpaspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaSpecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootjpaspec/SpringbootJpaSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjpaspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
