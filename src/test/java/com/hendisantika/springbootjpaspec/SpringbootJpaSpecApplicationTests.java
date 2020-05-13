9c1209f41ce19f9997af1f7a028a96acc52d2a51
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-13 08:08:03 +0700

    #20 - Modify Main Test

--- src/test/java/com/hendisantika/springbootjpaspec/SpringbootJpaSpecApplicationTests.java
+++ src/test/java/com/hendisantika/springbootjpaspec/SpringbootJpaSpecApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hendisantika.springbootjpaspec;
 
-import org.junit.jupiter.api.Test;
+import com.hendisantika.springbootjpaspec.repository.ClassRepository;
+import com.hendisantika.springbootjpaspec.repository.MemberRepository;
+import com.hendisantika.springbootjpaspec.spesification.MemberSpecification;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "spring.main.banner-mode=off")
+@Transactional
 class SpringbootJpaSpecApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private ClassRepository classRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberSpecification memberSpecification;
+
 
 }
